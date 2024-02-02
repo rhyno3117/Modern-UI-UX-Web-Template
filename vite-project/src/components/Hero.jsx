b9dc3685aa9e0f657d1be5fa6d84b988d2d15d27
ryan <rhynogolfer42@gmail.com>
2024-02-01 16:51:29 -0800

    Adds myfile.md

--- vite-project/src/components/Hero.jsx
+++ vite-project/src/components/Hero.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+import styles from '../style';
+import { discount, robot } from '../assets';
+
+const Hero = () => (
+  <section id="home" className={`flex md:flex-row fkex-col ${styles.paddingY}`}>
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <div className="flex flex-row items-center py-[6x] px-4 bg-discount-gradient rounded-[10px] mb-2">
+
+      </div>
+    </div>
+  </section>
+)
 
 export default Hero
