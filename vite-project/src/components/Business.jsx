99d5efb88aa56b4706be99bd31599ba73bbceb88
ryan <rhynogolfer42@gmail.com>
2024-02-06 12:44:03 -0800

    Adds myfile.md

--- vite-project/src/components/Business.jsx
+++ vite-project/src/components/Business.jsx
@@ -4,7 +4,14 @@ import Button from "./Button";
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="sm:block hidden" /> we’ll handle
+          the money.
+        </h2>
+      </div>
+    </section>
   )
 }
 
