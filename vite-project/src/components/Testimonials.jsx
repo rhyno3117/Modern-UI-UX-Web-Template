9ddb8e8c5ef6362b26fe09fda83e21903875acc9
ryan <rhynogolfer42@gmail.com>
2024-02-10 21:53:31 -0800

    Adds myfile.md

--- vite-project/src/components/Testimonials.jsx
+++ vite-project/src/components/Testimonials.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { feedback } from "../constants";
+import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
 
 const Testimonials = () => {
   return (
